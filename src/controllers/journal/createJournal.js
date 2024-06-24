6295624a1101cfbff80d3d4b26541141a4ef4932
ZinalPotphode <zinalvijaypotphode@gmail.com>
2024-06-24 13:40:50 +0530

    server.js modified and .env file added

--- src/controllers/journal/createJournal.js
+++ src/controllers/journal/createJournal.js
@@ -4,7 +4,8 @@ const createJournal = async (req, res) => {
     try {
         const data = await prisma.journal.create({
             data: {
-                year: req.body.year
+                year: req.body.year,
+                
             }
         });
         res.status(200).send("Done");
